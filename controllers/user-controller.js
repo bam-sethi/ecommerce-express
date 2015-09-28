71418f0ed8ec84e4fdb13c05919d6044320eb2ad
Ambar Sethi <ambarrrsethi@gmail.com>
2015-09-28 22:59:19 +0100

    commented out other routes and controller for now

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -20,6 +20,25 @@ router.get('/', function(req, res){
 router.get('/:id', function(req, res){
   User.findOne( { _id: req.params.id }, function(err, user){
     if(err) console.log(err);
-    res.json(user)
+  res.json(user)
   });
 });
+
+//CREATE
+router.post('/', function(req, res){
+
+  console.log(req.body);
+  var userData = req.body;
+  
+  var newUser = new User({
+    name: userData.name,
+    gender: userData.gender
+  });
+
+  newUser.save(function(err, user) {
+    if(err) console.log(err);
+    console.log('User has been created!');
+  res.json(user);
+
+  });
+})
